526daab9e367d7630933e991a9b0926edd85a9c2
unknown <riasat01@outlook.com>
2023-11-08 14:17:26 +0600

    books loaded based on query

--- index.js
+++ index.js
@@ -15,24 +15,48 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+
+        const library = client.db("library").collection("books");
+
+        // services related api endpoints
+        app.get('/books', async (req, res) => {
+            
+            const query = req.query;
+            // console.log(query);
+            if(query.category === 'All'){
+                const result = await library.find().toArray();
+                res.send(result);
+            }else{
+                const filter = { category: query.category };
+                const result = await library.find(filter).toArray();
+                res.send(result);
+            }
+        })
+        app.post('/books', async (req, res) => {
+            const book = req.body;
+            const result = await library.insertOne(book);
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
